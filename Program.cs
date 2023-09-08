4908e1ab1fa297890497aa76b3e6f59f7cf1344d
user <user@VUKKERT>
2023-09-08 14:56:14 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Stack;
+StackClass stack = new StackClass(10);
+for(int i = 0; i < stack.MaxSize; i++)
+{
+    stack.Push(i + 1);
+    Print(stack.Peek());
+}
+Console.WriteLine("\n");
+stack.Push(12); 
+Print(stack.Pop());
+Print(stack.Pop());
+Print(stack.Pop());
+
+StackClass clone = (StackClass)stack.Clone();
+
+Console.WriteLine("\n");
+Print(stack.Peek());
+Print(clone.Peek());
+
+StackClass Clone2 = new StackClass(stack);
+
+Console.WriteLine("\n");
+Print(stack.Peek());
+Print(Clone2.Peek());
+static void Print(int a)
+{
+    Console.WriteLine(a);
+}
---
+++ StackClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class StackClass : ICloneable
+    {
+        List<int> newList;
+        public int MaxSize;
+        private int Count = 0;
+        public object Clone() => MemberwiseClone();
+        public StackClass(int maxSize)
+        {
+            newList = new List<int>(maxSize);
+            MaxSize = maxSize;
+        }
+        public StackClass(StackClass obj)
+        {
+           MaxSize = obj.MaxSize;
+           newList = obj.newList;
+            Count = obj.Count;
+        }
+        public void Push(int value)
+        {
+            if(Count < MaxSize)
+            {
+                newList.Add(value);
+                Count++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+        }
+        public int Pop() 
+        {
+            int a = newList[Count-1];
+            newList.RemoveAt(Count -1);
+            Count--;
+            return a;
+        }
+        public int Peek()
+        {
+            return newList[Count - 1];
+        }
+    }
+}
